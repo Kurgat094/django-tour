25d7cbe00091edcdc03d5c5af42b2dd0644600c7
Kurgat094 <tobiaskipkogei@gmail.com>
2025-02-22 00:32:19 +0300

    tour site in django

---
+++ wildsafari/tour/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('/categories', categories, name='categories'),
+    path('/contests', contests, name='contests'),
+    path('/contestdetails', contestdetails, name='contestdetails'),
+    path('/users', users, name='users'),
+]
