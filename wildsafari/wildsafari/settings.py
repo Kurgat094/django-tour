e08540215f6a3f7bdc1a5063c9291e7422d01592
Kurgat094 <tobiaskipkogei@gmail.com>
2025-02-22 01:06:06 +0300

    all host

--- wildsafari/wildsafari/settings.py
+++ wildsafari/wildsafari/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-%&*mx7ujkx=wr1hcg!fy%(4(xv6boq3@)_h2sfcsez#0+(!0sd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
