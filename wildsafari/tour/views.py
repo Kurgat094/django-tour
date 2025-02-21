2e900cc49b344531b5229cd0a3e65ac79e7f5c57
Kurgat094 <tobiaskipkogei@gmail.com>
2025-02-22 00:32:19 +0300

    tour site in django

---
+++ wildsafari/tour/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'index.html')
+
+
+
+def categories(request):
+    return render(request, 'categories.html')
+
+def contests(request):
+    return render(request, 'contests.html')
+
+def contestdetails(request):
+    return render(request, 'contest-details.html')
+
+def users(request):
+    return render(request, 'users.html')
