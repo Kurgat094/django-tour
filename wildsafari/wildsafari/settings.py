29f203e1d77a6166caf7722a91baa103c40b2dec
Kurgat094 <tobiaskipkogei@gmail.com>
2025-02-22 07:36:27 +0300

    host allowed

--- wildsafari/wildsafari/settings.py
+++ wildsafari/wildsafari/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 import os
 
 PORT = os.environ.get("PORT", "10000")
-ALLOWED_HOSTS = ["https://django-tour-1.onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["https://django-tour-1.onrender.com","django-tour-1.onrender.com","localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -43,13 +43,13 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = "wildsafari.urls"
@@ -112,9 +112,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = 'static/'
+STATICFILES_DIRS=[
+    Path(BASE_DIR,'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_PORT=587
+EMAIL_HOST_USER="tobiaskipkogei@gmail.com"
+EMAIL_HOST_PASSWORD="mehvuoqhwudsdmxz"
+EMAIL_USE_TLS=True
+EMAIL_USE_SSL=False
+EMAIL_TIMEOUT = 10
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
